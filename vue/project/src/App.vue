f9bc70bd52ab599dd053eb07c1e757f5b193d81a
Nonconformiste <maison78901@gmail.com>
2024-05-27 02:39:40 +0300

    11

--- vue/project/src/App.vue
+++ vue/project/src/App.vue
@@ -15,6 +15,17 @@ import { RouterLink, RouterView } from 'vue-router'
   </nav>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+  nav {
+    width: 100%;
+    height: 73px;
+    background: #161616; 
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-radius: 25px 25px 0px 0px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  } 
 </style>
